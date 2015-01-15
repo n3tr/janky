cea1b0eaf8576d1f8acd5cfa09eec1486642342e
Jirat Ki <saakyz@gmail.com>
2015-01-15 18:50:06 +0700

    Hack chat service to include author

--- lib/janky/notifier/chat_service.rb
+++ lib/janky/notifier/chat_service.rb
@@ -5,13 +5,14 @@ module Janky
         status = build.green? ? "was successful" : "failed"
         color = build.green? ? "green" : "red"
 
-        message = "Build #%s (%s) of %s/%s %s (%ss) %s" % [
+        message = "Build #%s (%s) of %s/%s %s (%ss) by %s \n- %s" % [
           build.number,
           build.short_sha1,
           build.repo_name,
           build.branch_name,
           status,
           build.duration,
+          build.commit_author,
           build.web_url
         ]
 
